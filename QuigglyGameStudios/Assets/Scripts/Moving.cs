50fc9a2ad41d19d2616e6e5117c1ca2c6a0cf773
Genelink <bixbysfamily@gmail.com>
2024-03-11 15:28:30 -0700

    Started the project
    and now there is a player that moves

---
+++ QuigglyGameStudios/Assets/Scripts/Moving.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+
+    private float leftright;
+    private float updown;
+    private Rigidbody2D rb;
+    public float SPEED = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // getting the player input
+        leftright = Input.GetAxis("Horizontal");
+        updown = Input.GetAxis("Vertical");
+
+        // Adding speed to velocity depending on the player input.
+
+        rb.velocity = new Vector2(leftright * SPEED, updown * SPEED);
+
+
+
+
+    }
+}
